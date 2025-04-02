5deaab218789714ef3f5706b821d4d360b8b20b7
Esmailashraf <esmail5mostafa2@gmail.com>
2025-04-02 14:14:36 +0200

    Add project files.

---
+++ NZwalks.api/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZwalks.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+    public IActionResult GetAllStudent()
+        {
+            string[] students = ["esmail", "ahmed", "ali", "mohamed"];
+
+            return Ok(students);// return http with status 200
+
+        }
+    }
+}
---
+++ NZwalks.api/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.api.Models.Domain;
+
+namespace NZwalks.api.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Difficulty> difficulties { get; set; }
+        public DbSet<Region> regions { get; set; }
+
+        public DbSet<Walk> walks { get; set; }
+    }
+}
